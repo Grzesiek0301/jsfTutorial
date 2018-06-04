c27bd36e6930c611c3bc2ad8d2a904f29a528b9c
grzes <grzes@grzes-Komputer>
2018-06-04 20:30:12 +0200

    third commit

--- asdf/src/pl/urbanski/notowaniagieldowe/entity/Menedzer.java
+++ asdf/src/pl/urbanski/notowaniagieldowe/entity/Menedzer.java
@@ -12,11 +12,12 @@ public class Menedzer {
 	public Menedzer() {
 		spolki = new ArrayList<>(3);
 		Date d = new Date();
-		
+		// komentarz4
+//		komentarz pierwszy
 		spolki.add(new Spolka("Kocia spolka weglowa", "KSW", "energetyczny", 20.0, 11.1, d));
 		spolki.add(new Spolka("KitKompDotCom", "KKDC", "informatyczny", 90.0, 3.4, d));
 		spolki.add(new Spolka("Kocie dance studio", "sds", "rozrywkowy", 15.0, -11.7, d));
-		
+		//komentarz3
 		Date wczoraj =new Date(d.getTime()-86400000);
 		spolki.get(0).getNotowania().add(0,new Notowanie(18, wczoraj, 4.0, spolki.get(0)));
 		spolki.get(1).getNotowania().add(0,new Notowanie(87, wczoraj, -2.0, spolki.get(1)));
@@ -26,7 +27,7 @@ public class Menedzer {
 	public List<Spolka> getSpolki() {
 		return spolki;
 	}
-
+//komentarz
 	public void setSpolki(List<Spolka> spolki) {
 		this.spolki = spolki;
 	}
