c31adff9c937897c87d06d87388969258fb089dd
Grzesiek0301 <g.urbanski@op.pl>
2018-06-04 20:19:40 +0200

    second monday

--- asdf/src/pl/urbanski/notowaniagieldowe/entity/Menedzer.java
+++ asdf/src/pl/urbanski/notowaniagieldowe/entity/Menedzer.java
@@ -12,7 +12,7 @@ public class Menedzer {
 	public Menedzer() {
 		spolki = new ArrayList<>(3);
 		Date d = new Date();
-		
+//		komentarz pierwszy
 		spolki.add(new Spolka("Kocia spolka weglowa", "KSW", "energetyczny", 20.0, 11.1, d));
 		spolki.add(new Spolka("KitKompDotCom", "KKDC", "informatyczny", 90.0, 3.4, d));
 		spolki.add(new Spolka("Kocie dance studio", "sds", "rozrywkowy", 15.0, -11.7, d));
